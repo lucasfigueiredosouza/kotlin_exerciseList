0dfa8f35ff9deabd2bbee11d478002bf5e7a8162
lucasfigueiredosouza <lucas.figueiredo.souza@gmail.com>
2025-08-28 17:33:00 -0300

    List 1 start with Question 2 letter a, finished

--- src/L01Q02a.kt
+++ src/L01Q02a.kt
@@ -1,5 +1,19 @@
+
+
 fun main(){
     /*
-    *L01Q02a -
+    *L01Q02a - Write a program with read two notes,
+    *calculate average and show results
     * */
+
+    println("Informe a primeira nota :")
+    val nota1 = readln().toInt()
+
+    println("Informe a segunda nota :")
+    val nota2 = readln().toInt()
+
+    val media = (nota1+nota2)/2
+
+    println(media)
 }
+
