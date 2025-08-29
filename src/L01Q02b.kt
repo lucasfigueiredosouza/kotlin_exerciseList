01c7bfac8a542ee3b6a7bbfb0a75388668079835
lucasfigueiredosouza <lucas.figueiredo.souza@gmail.com>
2025-08-29 17:00:38 -0300

    completed

---
+++ src/L01Q02b.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+    println("Informe o primeiro nome")
+    val nome = readln()
+
+    println("Informe a idade")
+    val idade = readln()
+
+    //to print variable in println function use $variableName
+    println("$nome tem $idade anos")
+
+}
