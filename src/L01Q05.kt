d51f6bf83cda4cf069b8072f165dc9d9dcc5f180
lucasfigueiredosouza <lucas.figueiredo.souza@gmail.com>
2025-08-29 17:57:08 -0300

    completed

---
+++ src/L01Q05.kt
@@ -0,0 +1,8 @@
+fun main(){
+
+    var numerador: Float = (8F+9F+6F+8F+9F+7F)
+    var denominador: Float = 6F
+
+    val media: Float = numerador/denominador
+    println("A media aritmetica de 8,9,6,8,9,7 eh $media")
+}
