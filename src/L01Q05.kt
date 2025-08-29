a02b55a1ecc9635cbd7dd668089a9326aeba8b5f
lucasfigueiredosouza <lucas.figueiredo.souza@gmail.com>
2025-08-29 17:59:47 -0300

    completed

--- src/L01Q05.kt
+++ src/L01Q05.kt
@@ -1,5 +1,7 @@
 fun main(){
 
+    //use var for decimals numbers and letter F after number to set to decimal
+    //var for declare variable and val for values and operations
     var numerador: Float = (8F+9F+6F+8F+9F+7F)
     var denominador: Float = 6F
 
